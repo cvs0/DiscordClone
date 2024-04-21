305f752575c5e8e01f19e408faf7b09f23381e2f
cvs0 <143862758+cvs0@users.noreply.github.com>
2024-04-21 10:15:38 -0600

    Chat socket hook

--- discord-clone/components/chat/chat-messages.tsx
+++ discord-clone/components/chat/chat-messages.tsx
@@ -7,6 +7,7 @@ import { Loader2, ServerCrash } from "lucide-react";
 import { Fragment } from "react";
 import ChatItem from "./chat-item";
 import { format } from "date-fns";
+import { useChatSocket } from "@/hooks/use-chat-socket";
 
 const DATE_FORMAT = "d MMM yyyy, HH:mm"
 
@@ -27,7 +28,7 @@ interface ChatMessagesProps {
     apiUrl: string;
     socketUrl: string;
     socketQuery: Record<string, string>;
-    paramKey: "channelId" | "conversationid";
+    paramKey: "channelId" | "conversationId";
     paramValue: string;
     type: "channel" | "conversation";
 }
@@ -44,6 +45,9 @@ export const ChatMessages = ({
     type,
 }: ChatMessagesProps) => {
     const queryKey = `chat:${chatId}`
+    const addKey = `chat:${chatId}:messages`
+    const updateKey = `chat:${chatId}:messages:update`
+    
     const {
         data,
         fetchNextPage,
@@ -56,6 +60,7 @@ export const ChatMessages = ({
         paramKey,
         paramValue,
     })
+    useChatSocket({ queryKey, addKey, updateKey })
 
     if (status === "pending") {
         return (
--- discord-clone/hooks/use-chat-query.ts
+++ discord-clone/hooks/use-chat-query.ts
@@ -1,58 +1,41 @@
-import { useSocket } from "@/components/providers/socket-provider";
-import { useInfiniteQuery, UseInfiniteQueryOptions } from "@tanstack/react-query";
-import { useParams } from "next/navigation";
 import qs from "query-string";
+import { useInfiniteQuery } from "@tanstack/react-query";
+
+import { useSocket } from "@/components/providers/socket-provider";
 
 interface ChatQueryProps {
     queryKey: string;
     apiUrl: string;
-    paramKey: "channelId" | "conversationid";
+    paramKey: "channelId" | "conversationId";
     paramValue: string;
 }
 
-export const useChatQuery = ({
-    queryKey,
-    apiUrl,
-    paramKey,
-    paramValue,
-}: ChatQueryProps) => {
+export const useChatQuery = ({ queryKey, apiUrl, paramKey, paramValue }: ChatQueryProps) => {
     const { isConnected } = useSocket();
-    const params = useParams();
 
     const fetchMessages = async ({ pageParam = undefined }) => {
-        const url = qs.stringifyUrl({
-            url: apiUrl,
-            query: {
-                cursor: pageParam,
-                [paramKey]: paramValue
-            }
-        }, { skipNull: true });
+        const url = qs.stringifyUrl(
+            {
+                url: apiUrl,
+                query: {
+                    cursor: pageParam,
+                    [paramKey]: paramValue,
+                },
+            },
+            { skipNull: true }
+        );
 
         const res = await fetch(url);
-
         return res.json();
     };
 
-    const queryOptions: UseInfiniteQueryOptions<any, Error, any, any, any[], any> = {
+    const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } = useInfiniteQuery({
+        initialPageParam: undefined,
         queryKey: [queryKey],
         queryFn: fetchMessages,
         getNextPageParam: (lastPage) => lastPage?.nextCursor,
-        initialPageParam: undefined,
-    };
-
-    if (isConnected) {
-        queryOptions.refetchInterval = false;
-    } else {
-        queryOptions.refetchInterval = 1000;
-    }
-
-    const {
-        data,
-        fetchNextPage,
-        hasNextPage,
-        isFetchingNextPage,
-        status,
-    } = useInfiniteQuery(queryOptions);
+        refetchInterval: isConnected ? false : 1000,
+    });
 
     return {
         data,
